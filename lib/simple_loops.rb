971a7769dc9cd0a803d2a5fd0488eb32c7aea4a9
Charlie Knight <williamchknight@gmail.com>
2020-11-05 19:59:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,10 +25,12 @@ end
 
 def return_string_array(array)
   count = 0 
+  new_array = []
   while count < array.length do 
-    array[count].to_s
+    new_array.push(array[count].to_s)
     count += 1 
-  end   
+  end
+  new_array
 end     
     
     
