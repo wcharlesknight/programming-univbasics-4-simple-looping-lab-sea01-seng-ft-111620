345eaa5a3a78317557186fec480ed16bdc40cce7
Charlie Knight <williamchknight@gmail.com>
2020-11-05 19:43:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,12 @@ def loop_message_n_times(message, limit)
     count += 1 
   end 
 end 
+
+def output_array(array)
+  count = 0 
+  while count < array.length do 
+    puts 
+    count += 1 
+  end 
+end 
+
