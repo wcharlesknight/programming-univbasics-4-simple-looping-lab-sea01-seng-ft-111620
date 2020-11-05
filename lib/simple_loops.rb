8d8fbccb988acbd7bf6394b1dbd85d74d1913997
Charlie Knight <williamchknight@gmail.com>
2020-11-05 19:37:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,8 @@
 # Write your methods here
-def loop_message_five_times()
-  while counter < 5
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do 
+    puts message
+    count += 1 
+  end
 end 
